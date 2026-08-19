3969f4a4f66e6c715c65a59ce3d9dc20b4fdb218
Vaurel <svaurel@outlook.com>
2026-08-19 10:58:34 -0700

    Debug: Remove ESLint configuration
    Remove ESLint configuration and export an empty array.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,1 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
-
-export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-]);
+export default [];
